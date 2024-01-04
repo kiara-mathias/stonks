f49d494c88c6c830a8952db76dbd708f1153b03c
CodeSandbox <support@codesandbox.io>
2024-01-04 05:47:31 +0000

    Initial commit

--- dist/index.ac9dc4ba.js
+++ dist/index.ac9dc4ba.js
@@ -576,9 +576,45 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"aR1JP":[function(require,module,exports) {
 var _stylesCss = require("./styles.css");
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+var initialPrice = document.querySelector("#initial-price");
+var currentPrice = document.querySelector("#current-price");
+var stockQuantity = document.querySelector("#stock-quantity");
+var button = document.querySelector("#button");
+var output = document.querySelector("#output");
+button.addEventListener("click", buttonHandler);
+function buttonHandler() {
+    var ip = initialPrice.value;
+    var qty = stockQuantity.value;
+    var curr = currentPrice.value;
+    calculateProfitandLoss(ip, qty, curr);
+}
+function calculateProfitandLoss(initial, quantity, current) {
+    if (initial > current) {
+        var loss = initial - current;
+        var lossPercentage = loss * 100 / initial;
+        lossPercentage = parseInt(lossPercentage, 10);
+        // console.log(`loss ${loss} and ${lossPercentage}`);
+        colorChange("red");
+        showOutput(`Oops your in loss of ${loss} and your loss percentage is ${lossPercentage}%`);
+    } else if (current > initial) {
+        var profit = current - initial;
+        var profitPercentage = profit / initial * 100;
+        profitPercentage = parseInt(profitPercentage, 10);
+        colorChange("green");
+        showOutput(`YOUR IN PROFIT OF ${profit} and profit percentage is ${profitPercentage}%`);
+        console.log("profit");
+    } else {
+        console.log("no pain no gain");
+        colorChange("yellow");
+        showOutput(`NO PAIN NO GAIN`);
+    }
+}
+function colorChange(color) {
+    document.getElementById("output").style.color = color;
+}
+function showOutput(message) {
+    document.getElementById("output").innerHTML = message;
+}
 
 },{"./styles.css":"lW6qc"}],"lW6qc":[function() {},{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
 
--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,52 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+var initialPrice = document.querySelector("#initial-price");
+var currentPrice = document.querySelector("#current-price");
+var stockQuantity = document.querySelector("#stock-quantity");
+var button = document.querySelector("#button");
+var output = document.querySelector("#output");
+
+button.addEventListener("click", buttonHandler);
+
+function buttonHandler() {
+  var ip = initialPrice.value;
+
+  var qty = stockQuantity.value;
+
+  var curr = currentPrice.value;
+
+  calculateProfitandLoss(ip, qty, curr);
+}
+
+function calculateProfitandLoss(initial, quantity, current) {
+  if (initial > current) {
+    var loss = initial - current;
+    var lossPercentage = (loss * 100) / initial;
+    lossPercentage = parseInt(lossPercentage, 10);
+    // console.log(`loss ${loss} and ${lossPercentage}`);
+    colorChange("red");
+    showOutput(
+      `Oops your in loss of ${loss} and your loss percentage is ${lossPercentage}%`,
+    );
+  } else if (current > initial) {
+    var profit = current - initial;
+    var profitPercentage = (profit / initial) * 100;
+    profitPercentage = parseInt(profitPercentage, 10);
+    colorChange("green");
+    showOutput(
+      `YOUR IN PROFIT OF ${profit} and profit percentage is ${profitPercentage}%`,
+    );
+
+    console.log("profit");
+  } else {
+    console.log("no pain no gain");
+    colorChange("yellow");
+    showOutput(`NO PAIN NO GAIN`);
+  }
+}
+function colorChange(color) {
+  document.getElementById("output").style.color = color;
+}
+function showOutput(message) {
+  document.getElementById("output").innerHTML = message;
+}
